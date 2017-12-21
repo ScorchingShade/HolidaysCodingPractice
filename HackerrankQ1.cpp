9014523522fe390ba67798fb2dd1a959986f4e87
Ankush Sharma <ankushors789@gmail.com>
2017-12-21 16:13:40 +0530

    Hackerrank Q1

---
+++ HackerrankQ1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int simpleArraySum(int n, vector <int> ar) {
+    // Complete this function
+    int a,result=0;
+    for(int i=0;i<n;i++){
+        
+        cin>>a;
+        ar.push_back(n);
+        result+=ar[i];
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    int result = simpleArraySum(n, ar);
+    cout << result << endl;
+    return 0;
+}
